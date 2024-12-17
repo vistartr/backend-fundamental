8ef14f07d2a50d00e380ba31ec394cb47cce4ecb
vistartr <vistartr@gmail.com>
2024-12-17 23:13:33 +0700

    cekk

---
+++ NodeJsWebServices/body-request/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html'); 
+    response.statusCode = 200;
+
+    const { method } = request;
+
+    if(method === 'GET') {
+        response.end('<h1>HELLO</h1>');
+    }
+
+    if(method === 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.end(`<h1>CEKIDOUT INI POST, ${ name }!</h1>`);
+        });
+    }
+}
+
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
