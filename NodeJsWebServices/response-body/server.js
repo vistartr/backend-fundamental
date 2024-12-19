2cb26d2ebdc59db7dc397a53a4057d9798930575
vistartr <vistartr@gmail.com>
2024-12-19 21:16:54 +0700

    response-body

--- NodeJsWebServices/response-body/server.js
+++ NodeJsWebServices/response-body/server.js
@@ -13,17 +13,23 @@ const requestListener = (request, response) => {
     if(url === '/') {
         if(method === 'GET') {
             response.statusCode = 200;
-            response.end('<h1>Ini adalah homepage</h1>');
+            response.end(JSON.stringify({
+                message: 'Ini halaman homepage',
+            }));
         } 
         else {
             response.statusCode = 400;
-            response.end(`<h1> halaman tidak dapat diakses dengan method ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${ method } request `,
+            }));
         }    
     }   
     else if(url === '/about') {
         if(method === 'GET') {
             response.statusCode = 200;
-            response.end('<h1>Ini adalah halaman about</h1>');
+            response.end(JSON.stringify({
+                message: 'Ini halaman about',
+            }));
         } 
         else if(method === 'POST') {
             let body = [];
