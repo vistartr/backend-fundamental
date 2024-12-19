f5040eade86daeff4d52c4cf1bf9d9889f95466f
vistartr <vistartr@gmail.com>
2024-12-19 20:15:47 +0700

    change in server.js routing-request

---
+++ NodeJsWebServices/routing-request/server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+
+
+    const { url, method } = request;
+
+    
+    if(url === '/') {
+        if(method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        } 
+        else {
+            response.end(`<h1> halaman tidak dapat diakses dengan method ${method} request</h1>`);
+        }    
+    }   
+    else if(url === '/about') {
+        if(method === 'GET') {
+            response.end('<h1>Ini adalah halaman about</h1>');
+        } 
+        else if(method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Halo, ${name} ini adalah halaman about</h1>`);
+            });
+
+        } 
+        else {
+            response.end(`<h1>Halaman tidak dapat diakses menggunakan method ${method} request</h1>`);
+        }
+    }
+}
+
+const port = 5000;
+const host = 'localhost';
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+})
